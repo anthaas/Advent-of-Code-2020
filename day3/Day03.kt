ab4357128d5afb8bfa6d538b852cc503b4aea406
Antonin Haas <tondahaas@gmail.com>
2020-12-03 18:52:51 +0100

    small refactor

--- day3/Day03.kt
+++ day3/Day03.kt
@@ -3,22 +3,22 @@ import java.io.File
 const val TREE = '#'
 
 fun main(args: Array<String>) {
-    val forrest = File("input.txt").readLines().map { it.toCharArray() }
+    val forrest = File("input.txt").readLines()
 
     println(getTreeHits(forrest, 3, 1))
 
-    val slopeConfigurations = arrayListOf(Pair(1, 1), Pair(3, 1), Pair(5, 1), Pair(7, 1), Pair(1, 2))
-    val result = slopeConfigurations.map { getTreeHits(forrest, it.first, it.second) }.reduce { acc, l -> acc * l }
+    val slopeConfigurations = listOf(Pair(1, 1), Pair(3, 1), Pair(5, 1), Pair(7, 1), Pair(1, 2))
+    val result = slopeConfigurations.map { (x, y) -> getTreeHits(forrest, x, y) }.reduce { acc, l -> acc * l }
     println(result)
 }
 
-fun getTreeHits(forrest: List<CharArray>, shiftX: Int, shiftY: Int): Long {
+fun getTreeHits(forrest: List<String>, shiftX: Int, shiftY: Int): Long {
     var x = 0
     var y = 0
     var treeCount = 0L
 
     while (y < forrest.size) {
-        x %= forrest[0].size
+        x %= forrest[y].length
 
         if (forrest[y][x] == TREE) {
             treeCount++
