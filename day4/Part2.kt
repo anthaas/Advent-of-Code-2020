626bb2486d0c97049e1a25aef29593c6d5b525e2
Antonin Haas <tondahaas@gmail.com>
2020-12-04 18:05:55 +0100

    better validity condition for hgt

--- day4/Part2.kt
+++ day4/Part2.kt
@@ -6,7 +6,13 @@ fun main(args: Array<String>) {
         Pair("byr", { x: String -> x.toIntOrNull() in 1920..2002 }),
         Pair("iyr", { x: String -> x.toIntOrNull() in 2010..2020 }),
         Pair("eyr", { x: String -> x.toIntOrNull() in 2020..2030 }),
-        Pair("hgt", ::testHgt),
+        Pair("hgt", { x: String ->
+            when {
+                "cm" in x -> x.replace("cm", "").toIntOrNull() in 150..193
+                "in" in x -> x.replace("in", "").toIntOrNull() in 59..76
+                else -> false
+            }
+        }),
         Pair("hcl", { x: String -> x.matches(Regex("#[0-9,a-f]{6}")) }),
         Pair("ecl", { x: String -> x in listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth") }),
         Pair("pid", { x: String -> x.matches(Regex("[0-9]{9}")) })
@@ -15,18 +21,9 @@ fun main(args: Array<String>) {
     val valid = passports.map { passport ->
         passport.split("\r\n", " ").associate { it.split(":").let { (key, value) -> key to value } }
     }.map {
-        requiredFields.map { (value, validityCondition) -> it[value] != null && validityCondition(it[value]!!) }.all { it }
+        requiredFields.map { (value, validityCondition) -> it[value] != null && validityCondition(it[value]!!) }
+            .all { it }
     }.count { it }
 
     println(valid)
-}
-
-fun testHgt(x: String): Boolean {
-    if ("cm" in x) {
-        return x.replace("cm", "").toIntOrNull() in 150..193
-    } else if ("in" in x) {
-        return x.replace("in", "").toIntOrNull() in 59..76
-    }
-
-    return false
 }
