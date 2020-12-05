76af5384c500bd45f8a25cb33fcc0bd71974c267
Antonin Haas <tondahaas@gmail.com>
2020-12-05 17:48:33 +0100

    big refactor this time

--- day5/Part1.kt
+++ day5/Part1.kt
@@ -1,28 +1,9 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    val seats = File("input.txt").readLines()
-    val maxId = seats.map { getSeatId(it) }.max()
+    val seats =
+        File("input.txt").readLines().map { it.map { if (it == 'B' || it == 'R') '1' else '0' }.joinToString("") }
+    val maxId = seats.map { Integer.parseInt(it.substring(0, 7), 2) * 8 + Integer.parseInt(it.substring(7), 2) }.max()
     println(maxId)
-}
-
-private fun getSeatId(seat: String): Int {
-    return  getSeatRow(seat.substring(0,7), 0, 127) * 8 + getSeatColumn(seat.substring(7), 0, 7)
-}
-
-private fun getSeatRow(columnId: String, l: Int, r: Int): Int {
-    return when {
-        columnId.isEmpty() -> l
-        (columnId[0] == 'F') -> getSeatRow(columnId.substring(1), l, (l +r)/2)
-        else -> getSeatRow(columnId.substring(1), (l +r)/2+1, r)
-    }
-}
-
-private fun getSeatColumn(rowId: String, l: Int, r: Int): Int {
-    return when {
-        rowId.isEmpty() -> l
-        (rowId[0] == 'L') -> getSeatColumn(rowId.substring(1), l, (l +r)/2)
-        else -> getSeatColumn(rowId.substring(1), (l +r)/2+1, r)
-    }
 }
 
--- day5/Part2.kt
+++ day5/Part2.kt
@@ -1,33 +1,10 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    val seats = File("input.txt").readLines()
-    val seatsIds = seats.map { getSeatId(it) }
-    println(findSeat(seatsIds))
+    val seats =
+        File("input.txt").readLines().map { it.map { if (it == 'B' || it == 'R') '1' else '0' }.joinToString("") }
+    val seatsIds = seats.map { Integer.parseInt(it.substring(0, 7), 2) * 8 + Integer.parseInt(it.substring(7), 2) }
+    val mySeat = (seatsIds.min()!!..seatsIds.max()!!).minus(seatsIds)[0]
+    println(mySeat)
 }
 
-private fun findSeat(seatsIds: List<Int>): Int {
-    val min = seatsIds.min() ?: return 0
-    val max = seatsIds.max() ?: return 0
-    return (min..max).minus(seatsIds)[0]
-}
-
-private fun getSeatId(seat: String): Int{
-    return  getSeatRow(seat.substring(0,7), 0, 127) * 8 + getSeatColumn(seat.substring(7), 0, 7)
-}
-
-private fun getSeatRow(columnId: String, l: Int, r: Int): Int {
-    return when {
-        columnId.isEmpty() -> l
-        (columnId[0] == 'F') -> getSeatRow(columnId.substring(1), l, (l +r)/2)
-        else -> getSeatRow(columnId.substring(1), (l +r)/2+1, r)
-    }
-}
-
-private fun getSeatColumn(rowId: String, l: Int, r: Int): Int {
-    return when {
-        rowId.isEmpty() -> l
-        (rowId[0] == 'L') -> getSeatColumn(rowId.substring(1), l, (l +r)/2)
-        else -> getSeatColumn(rowId.substring(1), (l +r)/2+1, r)
-    }
-}
