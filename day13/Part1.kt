2428a5a74b90f83a0fc21f01335faff4ce80290a
Antonin Haas <tondahaas@gmail.com>
2020-12-13 14:16:45 +0100

    small refactor

--- day13/Part1.kt
+++ day13/Part1.kt
@@ -3,20 +3,9 @@ import java.io.File
 fun main(args: Array<String>) {
     val input = File("input.txt").readLines()
     val timestamp = input[0].toInt()
-    val busIds = input[1].split(",").filterNot { it == "x" }.map { it.toInt() }
-    val departments = busIds.map { busId -> busId to getMinimumGreaterThan(busId, timestamp) - timestamp }
-    val result = departments.minBy { it.second }!!.toList().reduce { acc, i -> acc * i }
+    val result = input[1].split(",").filterNot { it == "x" }.map { it.toInt() }.map { it to it - timestamp % it }
+        .minBy { it.second }!!.toList().reduce { acc, i -> acc * i }
     println(result)
 
 }
-
-private fun getMinimumGreaterThan(number: Int, limit: Int): Int {
-    var acc = 0
-    while (acc < limit) {
-        acc += number
-    }
-
-    return acc
-}
-
 
