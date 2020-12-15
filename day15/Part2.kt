b813de41cd54af9aeb4cd531838312bbb1ecc8e1
Antonin Haas <tondahaas@gmail.com>
2020-12-15 21:34:34 +0100

    faster algorithm

--- day15/Part2.kt
+++ day15/Part2.kt
@@ -1,16 +1,19 @@
 fun main(args: Array<String>) {
-    val indexMap = mutableMapOf<Int, MutableList<Int>>()
-    val input = mutableListOf(-1, 11, 18, 0, 20, 1, 7, 16)
-    input.forEachIndexed { index, i -> indexMap[i] = mutableListOf(index) }
-
-    (input.size..30000000).forEach { i ->
-        val result = when (indexMap[input.last()]!!.size) {
-            1 -> 0
-            else -> indexMap[input.last()]!!.takeLast(2).let { it[1] - it[0] }
+    val start = System.currentTimeMillis()
+    val indexMap = mutableMapOf<Int, Int>()
+    val input = listOf(11, 18, 0, 20, 1, 7, 16)
+    input.forEachIndexed { index, i -> indexMap[i] = index }
+    var last = input.last()
+    (input.size until 30000000).forEach { i ->
+        if (last !in indexMap) {
+            indexMap[last] = i - 1
+            last = 0
+        } else {
+            val distance = i - 1 - indexMap[last]!!
+            indexMap[last] = i - 1
+            last = distance
         }
-        input.add(result)
-        if (indexMap[result] == null) indexMap[result] = mutableListOf(i) else indexMap[result]!!.add(i)
     }
-
-    println(input.last())
+    println(last)
+    println(System.currentTimeMillis() - start)
 }
