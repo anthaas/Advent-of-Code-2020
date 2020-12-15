29b1b427c0f381ff8a64a3b9ebc91a34872b2af5
Antonín Haas <tondahaas@gmail.com>
2020-12-15 12:09:42 +0100

    add faster solution for part 2

--- day15/Part2.kt
+++ day15/Part2.kt
@@ -1,13 +1,30 @@
 fun main(args: Array<String>) {
-    val input = listOf(-1,11,18,0,20,1,7,16).toMutableList()
-    for( i in (input.size .. 30000000)) {
+    val input = mutableListOf(-1,11,18,0,20,1,7,16)
+    val indexMap = mutableMapOf<Int, MutableList<Int>>()
+    for (i in input) {
+        indexMap[i] = mutableListOf(input.indexOf(i))
+    }
+
+    for (i in (input.size..30000000)) {
         val lastNumber = input.last()
-        val lastIndex = input.lastIndexOf(lastNumber)
-        if (lastIndex == input.indexOf(lastNumber)) {
+        if (indexMap[lastNumber] == null) {
+            //first occurence
             input.add(0)
+            indexMap[lastNumber] = mutableListOf(i)
+        } else if (indexMap[lastNumber]!!.size == 1) {
+            //first occurence
+            input.add(0)
+            indexMap[0]!!.add(i)
         } else {
-            input.add(lastIndex - input.subList(0,lastIndex).lastIndexOf(lastNumber))
+            val distance = indexMap[lastNumber]!!.takeLast(2).let { it[1] - it[0] }
+            input.add(distance)
+            if (indexMap[distance] == null) {
+                indexMap[distance] = mutableListOf(i)
+            } else {
+                indexMap[distance]!!.add(i)
+            }
         }
+
         if (i % 100000 == 0) {
             println(i)
         }
