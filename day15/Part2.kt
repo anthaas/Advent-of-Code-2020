959ab03cc4501ee8cdae0076f6df57e70a38c041
Antonin Haas <tondahaas@gmail.com>
2020-12-15 20:30:47 +0100

    code refactor

--- day15/Part2.kt
+++ day15/Part2.kt
@@ -1,35 +1,16 @@
 fun main(args: Array<String>) {
-    val input = mutableListOf(-1,11,18,0,20,1,7,16)
     val indexMap = mutableMapOf<Int, MutableList<Int>>()
-    for (i in input) {
-        indexMap[i] = mutableListOf(input.indexOf(i))
-    }
+    val input = mutableListOf(-1, 11, 18, 0, 20, 1, 7, 16)
+    input.forEachIndexed { index, i -> indexMap[i] = mutableListOf(index) }
 
-    for (i in (input.size..30000000)) {
-        val lastNumber = input.last()
-        if (indexMap[lastNumber] == null) {
-            //first occurence
-            input.add(0)
-            indexMap[lastNumber] = mutableListOf(i)
-        } else if (indexMap[lastNumber]!!.size == 1) {
-            //first occurence
-            input.add(0)
-            indexMap[0]!!.add(i)
-        } else {
-            val distance = indexMap[lastNumber]!!.takeLast(2).let { it[1] - it[0] }
-            input.add(distance)
-            if (indexMap[distance] == null) {
-                indexMap[distance] = mutableListOf(i)
-            } else {
-                indexMap[distance]!!.add(i)
-            }
+    (input.size..30000000).forEach { i ->
+        val result = when (indexMap[input.last()]!!.size) {
+            1 -> 0
+            else -> indexMap[input.last()]!!.takeLast(2).let { it[1] - it[0] }
         }
-
-        if (i % 100000 == 0) {
-            println(i)
-        }
+        input.add(result)
+        if (indexMap[result] == null) indexMap[result] = mutableListOf(i) else indexMap[result]!!.add(i)
     }
 
     println(input.last())
-
 }
