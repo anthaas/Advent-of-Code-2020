d3f015da8f4fd933d5d589b3bc81231c5fa465ca
Antonin Haas <tondahaas@gmail.com>
2020-12-17 17:10:13 +0100

    refactoring

--- day17/Part1.kt
+++ day17/Part1.kt
@@ -10,43 +10,22 @@ fun main(args: Array<String>) {
                 .filter { it.isNotEmpty() }
         }.flatten()
 
-    aliveCells = evolve(aliveCells)
-    aliveCells = evolve(aliveCells)
-    aliveCells = evolve(aliveCells)
-    aliveCells = evolve(aliveCells)
-    aliveCells = evolve(aliveCells)
-    aliveCells = evolve(aliveCells)
-
+    for (i in (1..6)) {
+        aliveCells = evolve(aliveCells)
+    }
 
     println(aliveCells.size)
 }
 
-private fun evolve(aliveCells: List<List<Int>>): List<List<Int>> {
-    val maxX = aliveCells.map { it[0] }.max()!!
-    val minX = aliveCells.map { it[0] }.min()!!
-    val maxY = aliveCells.map { it[1] }.max()!!
-    val minY = aliveCells.map { it[1] }.min()!!
-    val maxZ = aliveCells.map { it[2] }.max()!!
-    val minZ = aliveCells.map { it[2] }.min()!!
-    val aliveForNextIter = mutableListOf<List<Int>>()
-    for (x in minX - 1..maxX + 1) {
-        for (y in minY - 1..maxY + 1) {
-            for (z in minZ - 1..maxZ + 1) {
-                val currentCell = listOf(x, y, z)
-                val neighbors = getNeighborsPosition(x, y, z)
-                val aliveNeighbors = neighbors.map { it in aliveCells }.count { it }
-                if (currentCell in aliveCells && (aliveNeighbors == 2 || aliveNeighbors == 3)) {
-                    aliveForNextIter.add(currentCell)
-                }
-                if (currentCell !in aliveCells && aliveNeighbors == 3) {
-                    aliveForNextIter.add(currentCell)
-                }
-            }
+private fun evolve(aliveCells: List<List<Int>>): List<List<Int>>  =
+    aliveCells.map { getNeighborsPosition(it[0], it[1], it[2]) }.flatten().distinct().mapNotNull {
+        val aliveNeighbors = getNeighborsPosition(it[0], it[1], it[2]).map { it in aliveCells }.count { it }
+        when {
+            (it in aliveCells && (aliveNeighbors == 2 || aliveNeighbors == 3)) -> it
+            (it !in aliveCells && aliveNeighbors == 3) -> it
+            else -> null
         }
     }
-
-    return aliveForNextIter
-}
 
 private fun getNeighborsPosition(x: Int, y: Int, z: Int): List<List<Int>> {
     val result = mutableListOf<List<Int>>()
