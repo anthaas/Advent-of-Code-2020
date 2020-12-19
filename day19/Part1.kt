9ab8fe788b4fa03ac3e01c0595d218336364c21b
Antonin Haas <tondahaas@gmail.com>
2020-12-19 18:07:36 +0100

    add solution for day 19 part1

---
+++ day19/Part1.kt
@@ -0,0 +1,23 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val input = File("input.txt").bufferedReader().use { it.readText() }.split("\n\n")
+    val rules = input[0].split("\n").associate { it.split(": ").let { (left, right) -> left.toInt() to right } }
+    val inputs = input[1].split("\n")
+    val result = convertToRegex(rules).let { regex -> inputs.count { regex.toRegex().matches(it) } }
+    println(result)
+}
+
+private fun convertToRegex(rules: Map<Int, String>): String {
+    var regexStr = rules[0]!!.removeSurrounding("\"").split(" ").let { listOf("(") + it + ")" }
+
+    while (regexStr.any { s -> s.toIntOrNull() != null }) {
+        regexStr = regexStr.map { s ->
+            s.toIntOrNull()?.let { n ->
+                rules[n]!!.removeSurrounding("\"").split(" ").let { listOf("(") + it + ")" }
+            } ?: listOf(s)
+        }.flatten()
+    }
+
+    return regexStr.joinToString("")
+}
